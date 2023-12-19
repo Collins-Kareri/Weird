553a353dd1dc8d255f319c6396e8b5ad230e9d56
c-kareri <139367760+c-kareri@users.noreply.github.com>
2023-12-19 19:26:43 +0300

    Feature: Search icon button used on small screens

---
+++ src/client/components/navigation/searchButtonIcon.tsx
@@ -0,0 +1,45 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+    IconButton,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalFooter,
+    Button,
+    useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import SearchBar from "../SearchBar";
+
+export default function SearchButtonIcon() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    return (
+        <>
+            <IconButton
+                icon={<SearchIcon />}
+                aria-label="search"
+                isRound={true}
+                onClick={onOpen}
+                display={{ base: "block", md: "none" }}
+            />
+
+            <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                <ModalOverlay />
+                <ModalContent marginX={3} paddingX={2}>
+                    <ModalHeader>Search Images</ModalHeader>
+                    <ModalCloseButton />
+                    <SearchBar />
+                    <ModalFooter className="tw-flex tw-gap-2">
+                        <Button variant={"ghost"} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button>Search</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
