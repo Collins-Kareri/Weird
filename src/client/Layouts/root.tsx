f42c2858ed37e4b230fedacf878c5deef8a15ec6
c-kareri <139367760+c-kareri@users.noreply.github.com>
2023-12-19 19:25:48 +0300

    Layout: Root layout logic

--- src/client/Layouts/root.tsx
+++ src/client/Layouts/root.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
 
 function Root() {
     return (
         <>
-            <nav>to be implemented</nav>
+            <Nav />
             <main>
                 <Outlet />
             </main>
