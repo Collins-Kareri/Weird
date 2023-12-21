fe5b8fab7e6d420ba0456e89499c43fcacc9d871
c-kareri <139367760+c-kareri@users.noreply.github.com>
2023-12-21 11:07:25 +0300

    UI: Added the support to change the input size

--- src/client/components/SearchBar.tsx
+++ src/client/components/SearchBar.tsx
@@ -1,14 +1,20 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import { InputGroup, InputLeftElement, Input, InputGroupProps } from "@chakra-ui/react";
+import { InputGroup, InputLeftElement, Input, InputGroupProps, InputProps } from "@chakra-ui/react";
 import React from "react";
 
-export default function SearchBar(props: InputGroupProps) {
+interface SearchBarProps extends InputGroupProps {
+    size?: InputProps["size"];
+}
+
+export default function SearchBar(props: SearchBarProps) {
+    const { size, ...rest } = props;
     return (
-        <InputGroup {...props}>
-            <InputLeftElement pointerEvents={"none"}>
+        <InputGroup {...rest}>
+            <InputLeftElement pointerEvents={"none"} className={size ? "!tw-top-[3px] !tw-left-[3px]" : ""}>
                 <SearchIcon boxSize={5} />
             </InputLeftElement>
-            <Input placeholder="search images" variant={"filled"} borderRadius={"lg"} />
+
+            <Input placeholder="search images" variant={"filled"} borderRadius={"lg"} type="text" size={size} />
         </InputGroup>
     );
 }
