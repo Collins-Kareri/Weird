78f95ec0b8d6ee3013b5ea5c08acfe5e27e07385
c-kareri <139367760+c-kareri@users.noreply.github.com>
2023-12-21 11:05:01 +0300

    UI: Added the template for image display

---
+++ src/client/components/gallery.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+export default function Gallery() {
+    return (
+        <div className="tw-grid tw-grid-cols-2 md:tw-grid-cols-4 tw-gap-4 tw-px-8 tw-py-8">
+            <div className="tw-grid tw-gap-4">
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
+                        alt=""
+                    />
+                </div>
+            </div>
+            <div className="tw-grid tw-gap-4">
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
+                        alt=""
+                    />
+                </div>
+            </div>
+            <div className="tw-grid tw-gap-4">
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
+                        alt=""
+                    />
+                </div>
+            </div>
+            <div className="tw-grid tw-gap-4">
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
+                        alt=""
+                    />
+                </div>
+                <div>
+                    <img
+                        className="tw-h-auto tw-max-w-full tw-rounded-lg"
+                        src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
+                        alt=""
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
