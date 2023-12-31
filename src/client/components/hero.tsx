98fe4fb6ece918725f5d128d24308a11199a458c
c-kareri <139367760+c-kareri@users.noreply.github.com>
2023-12-31 05:35:05 +0300

    UI: Hero section logic component

--- src/client/components/hero.tsx
+++ src/client/components/hero.tsx
@@ -1,21 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { LogoTypeFace } from "./Logo";
+// import makeRequest from "../utils/makeRequest";
+import { faker } from "@faker-js/faker";
+// import { Divider } from "@chakra-ui/react";
+
+// interface ImageProps {
+//     id: string;
+//     author: string;
+//     url: string;
+//     download_url: string;
+// }
 
 export default function Hero() {
+    const [backgroundImages, setBackgroundImages] = useState<string[] | [] | undefined>();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const images = [faker.image.urlLoremFlickr({ height: 960, width: 480 })];
+                // const images = await makeRequest("https://picsum.photos/1200/600");
+                console.log(images);
+                setBackgroundImages(images);
+            } catch (error) {
+                setBackgroundImages([]);
+            }
+        })();
+    }, []);
+
+    console.log(backgroundImages);
+
+    if (!backgroundImages) {
+        return <p>....Loading</p>;
+    }
+
     return (
-        <div className="tw-relative tw-flex tw-flex-col-reverse tw-py-16 lg:tw-pt-0 lg:tw-flex-col lg:tw-pb-0">
-            <div className="tw-inset-y-0 tw-top-0 tw-right-0 tw-z-0 tw-w-full tw-max-w-xl tw-px-4 tw-mx-auto md:tw-px-0 lg:tw-pr-0 lg:tw-mb-0 lg:tw-mx-0 lg:tw-w-7/12 lg:tw-max-w-full lg:tw-absolute xl:tw-px-0">
-                <img
-                    className="tw-object-cover tw-w-full tw-h-56 tw-rounded tw-shadow-lg lg:tw-rounded-lg lg:tw-shadow-none md:tw-h-96 lg:tw-h-full tw-opacity-80"
-                    src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                    alt=""
-                />
-                <p className="tw-text-right tw-bottom-2 tw-absolute tw-right-2 tw-hidden lg:tw-block">by username</p>
-            </div>
-            <div className="tw-relative tw-flex tw-flex-col tw-items-start tw-w-full tw-max-w-xl tw-px-4 tw-mx-auto tw-md:px-0 lg:tw-px-8 lg:tw-max-w-screen-xl">
-                <div className="tw-mb-16 lg:tw-my-40 lg:tw-max-w-lg lg:tw-pr-5">
+        <div className="tw-h-[80vh] tw-w-full tw-py-4 lg:tw-py-10 tw-relative">
+            <section className="tw-flex tw-flex-col tw-items-center tw-h-full tw-justify-center md:tw-justify-center md:tw-items-start tw-relative">
+                <section
+                    className={
+                        "tw-absolute tw-top-1/2 tw-left-1/2 -tw-translate-y-1/2 -tw-translate-x-1/2 tw-rounded-3xl -tw-z-[2] tw-h-3/4 tw-w-screen lg:tw-w-1/4 lg:tw-h-full tw-opacity-70 tw-ml-10 tw-overflow-clip tw-rounded-r-full lg:tw-z-0 lg:tw-left-[70%] lg:tw-translate-x-0 tw-bg-cover tw-bg-no-repeat tw-bg-center"
+                    }
+                    style={{ backgroundImage: `url(${backgroundImages[0]})` }}
+                ></section>
+                <div className="tw-w-full lg:tw-w-[60%] lg:tw-pl-10">
                     <LogoTypeFace className="tw-text-sm" />
                     <h2 className="tw-mb-1 tw-font-sans tw-text-3xl tw-font-bold tw-tracking-tight tw-text-gray-900 sm:tw-text-4xl sm:tw-leading-none">
                         Discover the Unusual.
@@ -29,7 +58,8 @@ export default function Hero() {
                         <Button variant={"outline"}>Browse collections</Button>
                     </ButtonGroup>
                 </div>
-            </div>
+            </section>
+            {/* <Divider className="tw-my-8" /> */}
         </div>
     );
 }
